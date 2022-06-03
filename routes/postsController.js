4d101f38517227f33011fff7307a1c0a7e221621
camille-granier <granierc@protonmail.com>
2022-06-03 19:26:32 +1000

    creating del request

--- routes/postsController.js
+++ routes/postsController.js
@@ -49,4 +49,17 @@ router.put('/:id', (req, res) => {
     )
 })
 
+router.delete('/:id', (req, res) =>{
+    if(!ObjectID.isValid(req.params.id))
+    return res.status(400).send("ID unknown: " + req.params.id)
+
+    PostsModel.findByIdAndRemove(
+        req.params.id,
+        (err, docs) => {
+            if(!err) res.send(docs);
+            else console.log("Delete error: " + err)
+        }
+    )
+})
+
 module.exports = router;
