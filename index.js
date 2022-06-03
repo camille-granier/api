c422448f736869837ba1236908f4a127b30e0b7c
camille-granier <granierc@protonmail.com>
2022-06-03 19:35:11 +1000

    adding cors package

--- index.js
+++ index.js
@@ -3,8 +3,11 @@ const app = express();
 require('./models/dbConfig');
 const postsRoutes = require('./routes/postsController');
 const bodyParser = require('body-parser');
+//package to provide middleware to enable access to data outside of given domain
+const cors = require('cors')
 
 app.use(bodyParser.json())
+app.use(cors());
 app.use('/posts', postsRoutes)
 
 app.listen(5500, () => console.log('server started: 5500'))
