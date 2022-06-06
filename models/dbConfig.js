3f373b0c8cb1e864e525fd784a603c006e3ec16e
camille-granier <granierc@protonmail.com>
2022-06-06 12:53:38 +1000

    removing deprecated command for Mongoose connect

--- models/dbConfig.js
+++ models/dbConfig.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 
 mongoose.connect(
     "mongodb://localhost:27017/node-api",
-    { useNewUrlParser: true, useUnifiedTopology: true},
     (err) => {
         if(!err) console.log("Mongodb connected");
         else console.log("Connexion error" + err);
